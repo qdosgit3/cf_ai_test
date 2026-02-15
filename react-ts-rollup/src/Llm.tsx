86d15319a955b5eb46b1d095e1446682b42df5f9
qdosgit3 <228497977+qdosgit3@users.noreply.github.com>
2026-02-15 19:29:18 +0000

    Update dLlm

--- react-ts-rollup/src/Llm.tsx
+++ react-ts-rollup/src/Llm.tsx
@@ -65,25 +65,40 @@ export async function call_llm(llm_req: string, llm_resp: string, set_llm_resp: 
 async function fetch_with_retries(llm_req: string, retry_count: number) {
 
 
-    // const ACCOUNT_ID = 'bfb0cd0115e98f74e4b00c41c72bac60';
-    // const API_TOKEN = 'gWxIKxQKCF4JDg5dpkv80hW9qBhLz1PNjpd03IDF';
+    const ACCOUNT_ID = 'bfb0cd0115e98f74e4b00c41c72bac60';
+    const API_TOKEN = 'gWxIKxQKCF4JDg5dpkv80hW9qBhLz1PNjpd03IDF';
 
-    const url = "https://hello-ai.qdosgit3.workers.dev/"
+    const url = `https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/ai/run/@cf/meta/llama-3.1-8b-instruct-fast`;
       
+    console.log(url);
+
     try {
 
-        const req = {
-        method: 'POST',
-        headers: {
+        req = {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${API_TOKEN}`,
         'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-        prompt: llm_req })
+    body: JSON.stringify({
+    "messages": [
+        {
+            "role":"system",
+            "content":"You are a friendly assistant that helps write stories"
+        },
+        {
+            "role":"user",
+            "content":"Write a short story about a llama that goes on a journey to find an orange cloud "
         }
+        ]
+      })
+    }
 
-        console.log(req)
+    console.log("sending22")
+
+    console.log(req)
 	
-        return await fetch(url, req).then(res => res.json());
+    return await fetch(url, req).then(res => res.json());
 
     } catch (error) {
 
