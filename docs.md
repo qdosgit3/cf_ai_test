687b6fd2c6900f62b17dd8ee9b48299242a317ee
qdosgit3 <228497977+qdosgit3@users.noreply.github.com>
2026-02-15 11:48:13 +0000

    Basic React app going via Codegauge guide

---
+++ docs.md
@@ -0,0 +1,4 @@
+
+Best minimalist React setup guide I found online, back in 2023:
+https://www.codeguage.com/blog/setup-rollup-for-react
+
---
+++ public/bundle.js
@@ -0,0 +1,10 @@
+(function (React, ReactDOM) {
+   'use strict';
+
+   function App() {
+     return /*#__PURE__*/React.createElement("h1", null, "Rollup is amazing!");
+   }
+
+   ReactDOM.createRoot(document.querySelector('#root')).render(/*#__PURE__*/React.createElement(App, null));
+
+})(React, ReactDOM);
